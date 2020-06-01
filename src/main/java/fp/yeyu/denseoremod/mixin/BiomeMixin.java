60c7a9b9d8f64ac9edfd938be3df240b923790fa
ye-yu <rafolwen98@gmail.com>
2020-06-01 18:31:11 +0800

    Update feature entry on feature generation

---
+++ src/main/java/fp/yeyu/denseoremod/mixin/BiomeMixin.java
@@ -0,0 +1,64 @@
+package fp.yeyu.denseoremod.mixin;
+
+import fp.yeyu.denseoremod.DenseOreMod;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.ChunkRandom;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.ChunkGeneratorConfig;
+import net.minecraft.world.gen.decorator.CountDepthDecoratorConfig;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+
+@Mixin(Biome.class)
+public abstract class BiomeMixin {
+    @Shadow @Final protected Map<GenerationStep.Feature, List<ConfiguredFeature<?, ?>>> features;
+
+    @Shadow public abstract void addFeature(GenerationStep.Feature step, ConfiguredFeature<?, ?> configuredFeature);
+
+    @Shadow @Final protected Biome.Category category;
+
+
+    @Inject(method="generateFeatureStep", at=@At("HEAD"), cancellable = true)
+    public void generateFeatureStepMixin(GenerationStep.Feature step, ChunkGenerator<? extends ChunkGeneratorConfig> chunkGenerator, IWorld world, long seed, ChunkRandom random, BlockPos pos, CallbackInfo ci) {
+        if (this.category != Biome.Category.PLAINS || world.getLevelProperties().getGeneratorType() != DenseOreMod.DENSE_ORE)
+            ci.cancel();
+
+        final Logger LOGGER = LogManager.getLogger(BiomeMixin.class);
+        LOGGER.info("Biome is plain. Generator type is Dense Ore. Restructuring ores.");
+
+        this.features.get(GenerationStep.Feature.UNDERGROUND_ORES).clear();
+
+        // from DefaultBiomeFeatures.addMineables
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.DIRT.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 256))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.GRAVEL.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(8, 0, 0, 256))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.GRANITE.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 80))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.DIORITE.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 80))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.ANDESITE.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(10, 0, 0, 80))));
+
+        final int amp = 5;
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.COAL_ORE.getDefaultState(), 17 * amp)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(5, 0, 0, 128))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.IRON_ORE.getDefaultState(), 9 * amp)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(5, 0, 0, 64))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.GOLD_ORE.getDefaultState(), 9 * amp)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(1, 0, 0, 32))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.REDSTONE_ORE.getDefaultState(), 8 * amp)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(2, 0, 0, 16))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.DIAMOND_ORE.getDefaultState(), 8 * amp)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(1, 0, 0, 16))));
+        this.addFeature(GenerationStep.Feature.UNDERGROUND_ORES, net.minecraft.world.gen.feature.Feature.ORE.configure(new OreFeatureConfig(OreFeatureConfig.Target.NATURAL_STONE, Blocks.LAPIS_ORE.getDefaultState(), 7 * amp)).createDecoratedFeature(Decorator.COUNT_DEPTH_AVERAGE.configure(new CountDepthDecoratorConfig(1, 16, 16))));
+    }
+
+}
