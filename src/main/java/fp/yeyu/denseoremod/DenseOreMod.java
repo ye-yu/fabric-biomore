4b00776b49d3822ec127785c4db6d8c59a229330
ye-yu <rafolwen98@gmail.com>
2020-06-01 12:32:10 +0800

    Make dense ore world type and integer type static

--- src/main/java/fp/yeyu/denseoremod/DenseOreMod.java
+++ src/main/java/fp/yeyu/denseoremod/DenseOreMod.java
@@ -9,22 +9,22 @@ import java.util.Objects;
 
 public class DenseOreMod implements ClientModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(DenseOreMod.class);
+    public static final int TYPE_EMPTY_AT;
+    public static final LevelGeneratorType DENSE_ORE;
 
-    public static LevelGeneratorType dense_ore;
-
-    @Override
-    public void onInitializeClient() {
-        LOGGER.info("Mod is loaded.");
-        addLevelType();
-    }
-
-    private static void addLevelType() {
+    static {
         int i = 0;
         //noinspection StatementWithEmptyBody
         while(Objects.nonNull(LevelGeneratorType.TYPES[i++]));
         i--; //reduce after increase when detecting null
-        LOGGER.info("Type is empty at: " + i);
-        dense_ore = (new LevelGeneratorType(i, "dense_ore", 2)).setVersioned();
-        LevelGeneratorType.TYPES[i] = dense_ore;
+        TYPE_EMPTY_AT = i;
+        DENSE_ORE = (new LevelGeneratorType(TYPE_EMPTY_AT, "dense_ore", 2)).setVersioned();
+        LOGGER.info("Type is empty at: " + i + ". Using this as the world level generation type integer.");
+        LevelGeneratorType.TYPES[i] = DENSE_ORE;
+    }
+
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("Mod is loaded.");
     }
 }
