badf730ad090018b000a3e5eca7021a03bdd359e
ye-yu <rafolwen98@gmail.com>
2020-06-03 06:18:47 +0800

    Refactor block comparison

--- src/main/java/fp/yeyu/denseoremod/feature/builder/targetfinder/BlockUtils.java
+++ src/main/java/fp/yeyu/denseoremod/feature/builder/targetfinder/BlockUtils.java
@@ -2,13 +2,10 @@ package fp.yeyu.denseoremod.feature.builder.targetfinder;
 
 import net.minecraft.block.Block;
 
-public class BlockUtils {
-    public static boolean isAmongBlock(Block[] blocks, Block block) {
-        for (Block testBlock : blocks) if (testBlock == block) return true;
-        return false;
-    }
+import java.util.Arrays;
 
+public class BlockUtils {
     public static boolean isAmongBlock(Block block, Block... blocks) {
-        return isAmongBlock(blocks, block);
+        return Arrays.asList(blocks).contains(block);
     }
 }
